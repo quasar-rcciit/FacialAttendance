0c367c9027e4a658287b42d68f67cf3a873a0bb3
Soham Banerjee <sohambanerjee4abc@hotmail.com>
2021-07-04 12:00:37 +0530

    linted

--- attendance.py
+++ attendance.py
@@ -4,17 +4,18 @@ import face_recognition
 import os
 from datetime import datetime
 
-path = 'ImageAttandance'
+path = "ImageAttandance"
 images = []
 classNames = []
 mylist = os.listdir(path)
 print(mylist)
 for cl in mylist:
-    currImg = cv2.imread(f'{path}/{cl}')
+    currImg = cv2.imread(f"{path}/{cl}")
     images.append(currImg)
     classNames.append(os.path.splitext(cl)[0])
 
 print(classNames)
+
 
 def findEncodings(images):
     encodeList = []
@@ -24,53 +25,58 @@ def findEncodings(images):
         encodeList.append(encode)
     return encodeList
 
+
 def markAttandance(name):
-    with open('Attandance.csv','r+') as f:
+    with open("Attandance.csv", "r+") as f:
         myDataList = f.readlines()
         nameList = []
         for line in myDataList:
-            entry=line.split(',')
+            entry = line.split(",")
             nameList.append(entry[0])
         if name not in nameList:
-            now =datetime.now()
-            tmString = now.strftime('%H:%M:%S')
-            dtString = now.strftime('%d/%m/%Y')
-            f.writelines(f'\n{name},{tmString},{dtString}')
+            now = datetime.now()
+            tmString = now.strftime("%H:%M:%S")
+            dtString = now.strftime("%d/%m/%Y")
+            f.writelines(f"\n{name},{tmString},{dtString}")
 
         print(myDataList)
 
 
-
-
-
-
 encodeListKnown = findEncodings(images)
-print('Encoding Complete')
-cap=cv2.VideoCapture(0)
+print("Encoding Complete")
+cap = cv2.VideoCapture(0)
 
 while True:
-    success , img=cap.read()
-    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    success, img = cap.read()
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
     facesCurrFrame = face_recognition.face_locations(imgS)
-    encodeCurrFrame = face_recognition.face_encodings(imgS,facesCurrFrame)
+    encodeCurrFrame = face_recognition.face_encodings(imgS, facesCurrFrame)
 
-    for encodeFace,faceLoc in zip(encodeCurrFrame,facesCurrFrame):
-        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
-        #print(faceDis)
+    for encodeFace, faceLoc in zip(encodeCurrFrame, facesCurrFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        # print(faceDis)
         matchIndex = np.argmin(faceDis)
 
         if matches[matchIndex]:
-            name=classNames[matchIndex].upper()
-            #print(name)
-            y1,x2,y2,x1=faceLoc
-            y1, x2, y2, x1=y1*4,x2*4,y2*4,x1*4
-            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,35),2)
-            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,35),cv2.FILLED)
-            cv2.putText(img,name,(x1+16,y2-6),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
+            name = classNames[matchIndex].upper()
+            # print(name)
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 35), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 35), cv2.FILLED)
+            cv2.putText(
+                img,
+                name,
+                (x1 + 16, y2 - 6),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (255, 255, 255),
+                2,
+            )
             markAttandance(name)
-    cv2.imshow('Webcam',img)
+    cv2.imshow("Webcam", img)
     cv2.waitKey(1)
 # faceLoc = face_recognition.face_locations(imgElon)[0]
 # encodeElon = face_recognition.face_encodings(imgElon)[0]
